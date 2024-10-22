f1d1fc478c8f842741c3617c5fc2279c1fad9be1
Mbluuka <asheludyankin@yandex.ru>
2024-10-22 13:31:50 +0300

    new

--- test.py
+++ test.py
@@ -1,46 +1,27 @@
 import json
 
-def get_common_auth_info():
-    file_to_check = "/etc/pam.d/common-auth"
+def get_common_password_info():
+    pam_cracklib_keyword = "pam_cracklib"
+    file_to_check = "/etc/pam.d/common-password"
     parameter_info = []
-    deny_value = None
 
     try:
         with open(file_to_check, 'r') as f:
             for line in f:
-                stripped_line = line.strip()
-                if not stripped_line or stripped_line.startswith('#'):
-                    continue
-
-                parts = stripped_line.split()
-
-                # Проверяем pam_tally.so и извлекаем deny
-                if 'pam_tally.so' in parts:
-                    for param in parts:
-                        if param.startswith('deny='):
-                            deny_value = param.split('=')[1]
-
-                # Проверяем параметры pam_cracklib
-                if 'pam_cracklib' in parts:
-                    parameters = parts[3:]  # Пропускаем первые три элемента
+                if pam_cracklib_keyword in line:
+                    parameters = line.strip().split()[3:]
                     param_dict = {}
                     for param in parameters:
-                        if '=' in param:
-                            key_value = param.split('=')
+                        key_value = param.split('=')
+                        if len(key_value) == 2:
                             param_dict[key_value[0]] = key_value[1]
                     parameter_info.append(param_dict)
-
     except Exception as e:
         return f"Ошибка при проверке файла {file_to_check}: {str(e)}"
 
-    if deny_value is not None:
-        parameter_info.append({'deny': deny_value})
-
     if parameter_info:
         return parameter_info
-    
-    return "Шаблон pam_cracklib не найден в файле common-auth."
-
+    return "Шаблон pam_cracklib не найден в файле common-password."
 
 
 def export_to_json(data, filename):
@@ -49,11 +30,11 @@ def export_to_json(data, filename):
 
 
 if __name__ == "__main__":
-    parameter_info = get_common_auth_info()
+    parameter_info = get_common_password_info()
 
     # Для более структурированного вывода login_defs
     if isinstance(parameter_info, list):
-        print("\nНайдены параметры pam_cracklib в common-auth:")
+        print("\nНайдены параметры pam_cracklib в common-password:")
         for params in parameter_info:
             for key, value in params.items():
                 print(f"{key}: {value}")
@@ -62,7 +43,7 @@ if __name__ == "__main__":
 
     export_to_json({
 
-        'login_defs': parameter_info
+        'common_password': parameter_info
 
     }, 'sys_info.json')
     
